f05601790c392f34f2d1eb0ad5a39bfc2c7c089e
Kellen Bell <93352579+kdb9520@users.noreply.github.com>
2023-11-18 17:49:31 -0500

    set up main file

---
+++ semester_project.py
@@ -0,0 +1,19 @@
+"""
+File:           semester_project.py
+Authors:        Kellen Bell (kdb9520), Derek Pruski (dmp3872)
+Description:    TODO
+"""
+
+import open3d as 3d
+import numpy as np
+
+"""
+main:   Runs the program
+params: none
+return: none
+"""
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
