f0e795b0aa03eef72bd86952755a48a9b69a9484
Derek Pruski <dmp3872@g.rit.edu>
2023-11-18 18:14:07 -0500

    open point cloud code

--- semester_project.py
+++ semester_project.py
@@ -4,7 +4,7 @@ Authors:        Kellen Bell (kdb9520), Derek Pruski (dmp3872)
 Description:    TODO
 """
 
-import open3d as 3d
+import open3d as o3d
 import numpy as np
 
 """
@@ -13,7 +13,14 @@ params: none
 return: none
 """
 def main():
-    pass
+    ply_point_cloud = o3d.data.PLYPointCloud()
+    pcd = o3d.io.read_point_cloud('dataset\\PointClouds\\0.pcd')
+    print(np.asarray(pcd.points))
+    o3d.visualization.draw_geometries([pcd],
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
 
 if __name__ == "__main__":
     main()
