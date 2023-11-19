5655dd63a1322309409190f3d700857c8b4dabc9
Kellen Bell <93352579+kdb9520@users.noreply.github.com>
2023-11-19 15:54:54 -0500

    distance between points

--- semester_project.py
+++ semester_project.py
@@ -15,13 +15,21 @@ return: none
 def main():
     # open pointCloud and view
     ply_point_cloud = o3d.data.PLYPointCloud()
-    pcd = o3d.io.read_point_cloud('dataset\\PointClouds\\0.pcd')
-    print(np.asarray(pcd.points))
-    o3d.visualization.draw_geometries([pcd],
-                                  zoom=0.3412,
-                                  front=[0.4257, -0.2125, -0.8795],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])
+    pcd0 = o3d.io.read_point_cloud('dataset\\PointClouds\\0.pcd')
+    pcd1 = o3d.io.read_point_cloud('dataset\\PointClouds\\1.pcd')
+
+    distances = pcd0.compute_point_cloud_distance(pcd1)
+
+    for i, dist in enumerate(distances):
+        if dist > 0.0009:
+            print(f"Point {i}: Distance = {dist:.4f}")
+
+    # print(np.asarray(pcd.points))
+    # o3d.visualization.draw_geometries([pcd],
+    #                               zoom=0.3412,
+    #                               front=[0.4257, -0.2125, -0.8795],
+    #                               lookat=[2.6172, 2.0475, 1.532],
+    #                               up=[-0.0694, -0.9768, 0.2024])
 
 if __name__ == "__main__":
     main()
