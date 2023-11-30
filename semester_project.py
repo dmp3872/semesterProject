631ef5b8dd2ad532771c09be9e5ac5e2ddab1468
Derek Pruski <dmp3872@g.rit.edu>
2023-11-29 19:46:44 -0500

    clustering template

--- semester_project.py
+++ semester_project.py
@@ -6,6 +6,7 @@ Description:    TODO
 
 import open3d as o3d
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 main:   Runs the program
@@ -15,17 +16,30 @@ return: none
 def main():
     # open pointCloud and view
     ply_point_cloud = o3d.data.PLYPointCloud()
-    pcd0 = o3d.io.read_point_cloud('dataset\\PointClouds\\0.pcd')
-    pcd1 = o3d.io.read_point_cloud('dataset\\PointClouds\\1.pcd')
+    # pcd0 = o3d.io.read_point_cloud('dataset\\PointClouds\\0.pcd')
+    # pcd1 = o3d.io.read_point_cloud('dataset\\PointClouds\\1.pcd')
+    # pcd2 = o3d.io.read_point_cloud('dataset\\PointClouds\\2.pcd')
 
-    distances = pcd0.compute_point_cloud_distance(pcd1)
+    # distances = pcd0.compute_point_cloud_distance(pcd1)
+    # distances2 = pcd1.compute_point_cloud_distance(pcd2)
 
-    for i, dist in enumerate(distances):
-        if dist > 0.0009:
-            print(f"Point {i}: Distance = {dist:.4f}")
+    pcd = o3d.io.read_point_cloud("dataset\\PointClouds\\401.pcd")
 
-    # print(np.asarray(pcd.points))
-    # o3d.visualization.draw_geometries([pcd],
+    with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
+        labels = np.array(pcd.cluster_dbscan(eps=0.02, min_points=10, print_progress=True))
+
+        max_label = labels.max()
+        print(f"point cloud has {max_label + 1} clusters")
+        colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+        colors[labels < 0] = 0
+        pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
+        o3d.visualization.draw_geometries([pcd], zoom=0.455,
+                                        front=[-0.4999, -0.1659, -0.8499],
+                                        lookat=[2.1813, 2.0619, 2.0999],
+                                        up=[0.1204, -0.9852, 0.1215])
+
+    # print(np.asarray(pcd0.points))
+    # o3d.visualization.draw_geometries([pcd0],
     #                               zoom=0.3412,
     #                               front=[0.4257, -0.2125, -0.8795],
     #                               lookat=[2.6172, 2.0475, 1.532],
